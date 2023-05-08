7f16e38f9bd4fb5f7a9b04bd1c3b837d46f370d4
David Lai <pf.davidlai@gmail.com>
2023-05-07 22:41:20 -0700

    create sessionforms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,17 +7,25 @@ import MainPage from './components/MainPage/MainPage';
 import LoginForm from './components/SessionForms/LoginForm';
 import SignupForm from './components/SessionForms/SignupForm';
 
+import { getCurrentUser } from './store/session';
+
 function App() {
-  return (
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCurrentUser()).then(() => setLoaded(true));
+  }, [dispatch]);
+
+  return loaded && (
     <>
-    <NavBar />
+      <NavBar />
       <Switch>
         <AuthRoute exact path="/" component={MainPage} />
         <AuthRoute exact path="/login" component={LoginForm} />
         <AuthRoute exact path="/signup" component={SignupForm} />
       </Switch>
     </>
-    );
+  );
 }
 
 export default App;
