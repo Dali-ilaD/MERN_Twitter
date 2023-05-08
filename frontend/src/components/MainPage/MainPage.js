6b67596c1b0b8d0b9c8f1a737d336b5ff7700d88
David Lai <pf.davidlai@gmail.com>
2023-05-07 22:10:53 -0700

    create NavBar

---
+++ frontend/src/components/MainPage/MainPage.js
@@ -0,0 +1,15 @@
+
+
+
+function MainPage() {
+    return (
+      <>
+        <p>A Twitter Clone</p>
+        <footer>
+          Copyright &copy; 2022 Chirper
+        </footer>
+      </>
+    );
+  }
+  
+  export default MainPage;
